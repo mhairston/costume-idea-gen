481162b0278b27230b2cce4f7a232735dce60d04
Michael Hairston <michael.hairston@cengage.com>
2013-10-27 13:52:22 -0400

    More vocabulary (nsfw), layout and style tweaks

--- vocab.js
+++ vocab.js
@@ -1,18 +1,41 @@
 // Vocabulary for Halloween Costume Idea Generator
 
-
 // define adjectives - Modifier for the character
-g1 = "Zombie*Robot*Sexy*Dizzy*Cannibal*Overweight*Frozen*Sweaty*8-foot tall*Creepy*Amnesiac*Alien*Midget*Blind*Drunk*<small>LEGO&trade;</small>*Bald*Incontinent*Spastic*Baby*Constipated*Hunchback*Handcuffed*Yodelling*Gargling*Shrunken*Slime-covered*Stuttering*Hungover*Hobo*Mechanical*Unshaven*Fat";
+g1 = "Zombie*Robot*Sexy*Disoriented*Cannibal*Stoned*Overweight*Frozen*Sweaty*8-foot tall*Creepy*Amnesiac"
+  + "*Midget*Blind*Drunk*<small>LEGO&trade;</small>*Gay*Bald*Stinky*Incontinent*Clumsy*Spastic*Blonde"
+  + "*Baby*Constipated*Hunchback*Handcuffed*Jaundiced*Masturbating*Yodelling*Gargling*Shrunken*Slime-covered*Stuttering"
+  + "*Hungover*Hobo*Mechanical*Bug-eyed*Brain-damaged*Unshaven*Fat*Cock-eyed*Cross-eyed*Nearsighted";
 
 // define objects - the basic character the costume is based on
-g2 = "Ben Franklin*Khaddafi*Bill Clinton*Darth Vader*Inigo Montoya*Ghost*Devil*Zorro*Dracula*Frankenstein*Smurf*Terminator*Indiana Jones*Princess Leia*Mr.  Spock*Captain Kirk*Napoleon*Menudo*Zebra*Dr. Who*Dog Catcher*Centaur*Angel*Medieval Knight*Voldemort*Galdalf*Harry Potter*Bilbo Baggins*Batman & Robin*Three Stooges*Mario*Jason*Joker*Headless Horseman*Disembodied Head*Chewbacca*Doc Brown*George Washington*Luigi*Superman*Monkey*Yoda*Skeleton*Dolphin*Goblin*Spiderman*Hulk*Dumbledore*Optimus Prime*King Arthur*Sir Lancelot*Squirrel*Exorcist*Garfield*Priest*Big Bird*Elmo*Kermit*Count*Penguin*Joe Camel*Grinch*Mr Rogers*James Bond*Porcupine*Platypus*Popeye*Pirate*Elf*ALF*Clown*Tree*Bee*Firefighter*Horse*Hobbit*Sauron*Wicked Witch*Dr. Evil*Fairy*Leprechaun*Grim Reaper*Amoeba*Vampire*Hyena*Werewolf*Yeti*Frenchman*Mayor McCheese*Burger King*Policeman*Surgeon*Mormon Tabernacle Choir*Sloth*Yogi Bear*Tony the Tiger*Captain Picard";
+g2 = "Ben Franklin*Khaddafi*Bill Clinton*Darth Vader*Inigo Montoya*Ghost*Pan*Zorro*Dracula"
+  + "*Frankenstein*Smurf*Terminator*Indiana Jones*Princess Leia*Mr. Spock*Captain Kirk*Satan"
+  + "*Napoleon*Menudo*Zebra*Dr. Who*Dalek*Dog Catcher*Centaur*Angel*Medieval Knight*Voldemort*Galdalf"
+  + "*Harry Potter*Alien*Bilbo Baggins*Batman & Robin*Three Stooges*Mario*Jason*Joker"
+  + "*Headless Horseman*Disembodied Head*Lex Luthor*Freddy Kruger*Cheerleader*Chuck Norris*Rabbi"
+  + "*Chewbacca*Doc Brown*George Washington*Luigi*Superman*R2D2*Wonder Woman*Catwoman"
+  + "*Monkey*Yoda*Skeleton*Dolphin*Goblin*Spiderman*Hulk*Dumbledore*Optimus Prime*King Arthur"
+  + "*Sir Lancelot*Squirrel*Exorcist*Garfield*Dementor*Priest*Cat*Monica Lewinsky*Big Bird*Elmo*Kermit*Count*Penguin"
+  + "*Joe Camel*Minion*Lois Lane*Grinch*Mr Rogers*James Bond*Porcupine*Platypus*Popeye*Pirate*Elf*ALF*Clown*Tree*Bee"
+  + "*Firefighter*Horse*Hobbit*Sauron*Wicked Witch*Dr. Evil*Fairy*Leprechaun*Grim Reaper"
+  + "*Amoeba*Vampire*Hyena*Werewolf*Yeti*Frenchman*Mayor McCheese*Burger King"
+  + "*Policeman*Surgeon*Mormon Tabernacle Choir*Sloth*Yogi Bear*Tony the Tiger*Captain Picard";
 
 // define state1 - conditions, dispositions, or things they have with them
-g3a = "running in circles*hopping on one foot*on a skateboard*on a scooter*with no short-term memory*with Tourette's*on fire*hitting on everyone*from Heck*with terrible acne*with no pants*with no eyebrows*with no mouth*with no nose*with a monocle*in a tuxedo*in a diving outfit*in a spacesuit*playing a banjo*in a bikini*in a prom dress*with an afro*with a tiny head*in a pith helmet*in a viking helmet*in a zoot suit*with antlers*playing the triangle*with tentacles*with a lisp*in pumps*with a scarf*eating a burrito*chewing gum*in a body cast*with braces*on a unicycle*on a tricycle*in a phone booth*playing the harmonica*in a miniskirt*in a kilt*in lederhosen";
+g3a = "running in circles*hopping on one foot*on a skateboard*on a scooter*with no short-term memory"
+  + "*with Tourette's*on fire*hitting on everyone*from Heck*with terrible acne*with no pants"
+  + "*with no eyebrows*with no mouth*with no nose*with a monocle*in a tuxedo*in a diving outfit*in a spacesuit"
+  + "*playing a banjo*in a bikini*in a prom dress*with an afro*with a tiny head*in a pith helmet"
+  + "*in a viking helmet*in a zoot suit*with antlers*playing the triangle*with tentacles*with a lisp"
+  + "*in pumps*with a scarf*eating a burrito*chewing gum*in a body cast*with braces*on a unicycle"
+  + "*on a tricycle*in a phone booth*playing the harmonica*in a miniskirt*in a kilt*in lederhosen*with goggles";
+  + "*with an arrow through the head*in a Tardis*with a black eye*eating a turkey leg*with a dickie";
 
 // define state2 - things that can substitute for body parts
 // (first item blank)
-g3b = "a mohawk*a ski pole*a goatee*muttonchops*a tennis racket*a basketball hoop*a machete*a hook*a chainsaw*a scythe*an eggbeater*hedge-clippers*a rubber duck*a baseball bat*a pizza box*a turkey leg*a hoola hoop*a beer keg*a Rubik's Cube*a cigar*a megaphone*a pineapple*an axe*a trebuchet*a bottle of Jim Beam";
+g3b = "a mohawk*a ski pole*a goatee*muttonchops*a tennis racket*a lawn mower*a basketball hoop*a machete"
+  + "*a hook*a chainsaw*a scythe*an eggbeater*hedge-clippers*a blowtorch*a rubber duck*a baseball bat"
+  + "*a pizza box*a hoola hoop*a beer keg*a Rubik's Cube*a cigar*a megaphone"
+  + "*a pineapple*an axe*a trebuchet*a bottle of Jim Beam*with a bong";
 
 // define substitution - body part to be substituted
 g3b2 = "for a hand*for a foot*for a head";
