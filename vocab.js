dc005cbe3bf26c78c265cc705a3c234e4da37433
Michael Hairston <michael.hairston@cengage.com>
2013-10-27 18:59:00 -0400

    Updates to vocabulary, styles

--- vocab.js
+++ vocab.js
@@ -1,41 +1,49 @@
 // Vocabulary for Halloween Costume Idea Generator
 
 // define adjectives - Modifier for the character
-g1 = "Zombie*Robot*Sexy*Disoriented*Cannibal*Stoned*Overweight*Frozen*Sweaty*8-foot tall*Creepy*Amnesiac"
-  + "*Midget*Blind*Drunk*<small>LEGO&trade;</small>*Gay*Bald*Stinky*Incontinent*Clumsy*Spastic*Blonde"
-  + "*Baby*Constipated*Hunchback*Handcuffed*Jaundiced*Masturbating*Yodelling*Gargling*Shrunken*Slime-covered*Stuttering"
-  + "*Hungover*Hobo*Mechanical*Bug-eyed*Brain-damaged*Unshaven*Fat*Cock-eyed*Cross-eyed*Nearsighted";
+g1 = "Undead*Robot*Sexy*Disoriented*Cannibal*Stoned*Overweight*Frozen*8-foot tall*Creepy*Amnesiac"
+  + "*Midget*Blind*Drunk*<small>LEGO</small>*Gay*Bald*Stinky*Incontinent*Clumsy*Spastic*Blonde"
+  + "*Baby*Constipated*Hunchback*Handcuffed*Jaundiced*Masturbating*Yodelling*Gargling*Shrunken"
+  + "*Slime-covered*Stuttering*Cock-eyed*Cross-eyed*Nearsighted*Unemployed"
+  + ""
+  + "*Hungover*Hobo*Mechanical*Bug-eyed*Metal*Marshmallow*Wooden*Brain-damaged*Sleep-deprived";
 
 // define objects - the basic character the costume is based on
 g2 = "Ben Franklin*Khaddafi*Bill Clinton*Darth Vader*Inigo Montoya*Ghost*Pan*Zorro*Dracula"
   + "*Frankenstein*Smurf*Terminator*Indiana Jones*Princess Leia*Mr. Spock*Captain Kirk*Satan"
-  + "*Napoleon*Menudo*Zebra*Dr. Who*Dalek*Dog Catcher*Centaur*Angel*Medieval Knight*Voldemort*Galdalf"
-  + "*Harry Potter*Alien*Bilbo Baggins*Batman & Robin*Three Stooges*Mario*Jason*Joker"
-  + "*Headless Horseman*Disembodied Head*Lex Luthor*Freddy Kruger*Cheerleader*Chuck Norris*Rabbi"
-  + "*Chewbacca*Doc Brown*George Washington*Luigi*Superman*R2D2*Wonder Woman*Catwoman"
-  + "*Monkey*Yoda*Skeleton*Dolphin*Goblin*Spiderman*Hulk*Dumbledore*Optimus Prime*King Arthur"
-  + "*Sir Lancelot*Squirrel*Exorcist*Garfield*Dementor*Priest*Cat*Monica Lewinsky*Big Bird*Elmo*Kermit*Count*Penguin"
-  + "*Joe Camel*Minion*Lois Lane*Grinch*Mr Rogers*James Bond*Porcupine*Platypus*Popeye*Pirate*Elf*ALF*Clown*Tree*Bee"
-  + "*Firefighter*Horse*Hobbit*Sauron*Wicked Witch*Dr. Evil*Fairy*Leprechaun*Grim Reaper"
-  + "*Amoeba*Vampire*Hyena*Werewolf*Yeti*Frenchman*Mayor McCheese*Burger King"
-  + "*Policeman*Surgeon*Mormon Tabernacle Choir*Sloth*Yogi Bear*Tony the Tiger*Captain Picard";
+  + "*Napoleon*Menudo*Zebra*Dr. Who*Dalek*Dog Catcher*Centaur*Medieval Knight*Voldemort*Galdalf"
+  + "*Harry Potter*Alien*Bilbo*Frodo*Gimli*Legolas*E.T.*Batman & Robin*Three Stooges*Mario*Jason*Joker"
+  + "*Headless Horseman*Darth Maul*Disembodied Head*Cujo*Lex Luthor*Ogre*Chicken*Freddy Kruger"
+  + "*Cheerleader*Chuck Norris*Rabbi*Wonder Woman*Catwoman*Pirate*Elf*ALF*Clown*Bee"
+  + "*Chewbacca*Doc Brown*Michael Jackson*George Washington*Sailor*Stormtrooper*Luigi*Superman*R2D2"
+  + "*Monkey*Yoda*Skeleton*Hamster*Pikachu*Anime Princess*Dolphin*Goblin*Spiderman*Hulk*Dumbledore"
+  + "*Sir Lancelot*Squirrel*Exorcist*Garfield*Mannequin*Dementor*Priest*Cat*Monica Lewinsky*Big Bird"
+  + "*Joe Camel*Minion*Lois Lane*Zombie*Grinch*Mr Rogers*James Bond*Porcupine*Platypus*Popeye"
+  + "*Easter Bunny*Santa*Tooth Fairy*Sandman*Hobbit*Sauron*Wicked Witch*Dr. Evil"
+  + "*Amoeba*Vampire*Hyena*Werewolf*Yeti*Frenchman*Godzilla*King Kong*Mayor McCheese*Burger King"
+  + "*Fairy*Leprechaun*Grim Reaper*Elmo*Kermit*Count*Penguin*King Arthur*Optimus Prime"
+  + ""
+  + "*Policeman*Surgeon*Mormon Tabernacle Choir*Orangutan*Yogi Bear*Tony the Tiger*Captain Picard";
 
 // define state1 - conditions, dispositions, or things they have with them
-g3a = "running in circles*hopping on one foot*on a skateboard*on a scooter*with no short-term memory"
+g3a = "running in circles*with a pegleg*on a skateboard*on a scooter*with no short-term memory"
   + "*with Tourette's*on fire*hitting on everyone*from Heck*with terrible acne*with no pants"
-  + "*with no eyebrows*with no mouth*with no nose*with a monocle*in a tuxedo*in a diving outfit*in a spacesuit"
-  + "*playing a banjo*in a bikini*in a prom dress*with an afro*with a tiny head*in a pith helmet"
-  + "*in a viking helmet*in a zoot suit*with antlers*playing the triangle*with tentacles*with a lisp"
+  + "*with no face*with a monocle*with googley eyes*in a tuxedo*in a diving outfit"
+  + "*playing a banjo*in a bikini*with an afro*with a tiny head*in a pith helmet"
+  + "*in a viking helmet*in a zoot suit*with antlers*playing the triangle*playing the bouzouki"
+  + "*playing the accordion*with tentacles*with two heads*with four arms*with a lisp"
   + "*in pumps*with a scarf*eating a burrito*chewing gum*in a body cast*with braces*on a unicycle"
-  + "*on a tricycle*in a phone booth*playing the harmonica*in a miniskirt*in a kilt*in lederhosen*with goggles";
-  + "*with an arrow through the head*in a Tardis*with a black eye*eating a turkey leg*with a dickie";
+  + "*on a tricycle*in a phone booth*playing the harmonica*in a miniskirt*in a kilt"
+  + "*in lederhosen*with goggles*in a spacesuit*with a dickie*in a tuque*in a propellor beanie";
+  + "*in a cowboy hat*in a tornado*with bunny ears"
+  + "*with an arrow through the head*in a Tardis*on skis*with a black eye*eating a turkey leg";
 
 // define state2 - things that can substitute for body parts
 // (first item blank)
-g3b = "a mohawk*a ski pole*a goatee*muttonchops*a tennis racket*a lawn mower*a basketball hoop*a machete"
-  + "*a hook*a chainsaw*a scythe*an eggbeater*hedge-clippers*a blowtorch*a rubber duck*a baseball bat"
+g3b = "a mohawk*a goatee*muttonchops*a tennis racket*a lawn mower*a basketball hoop*a boomerang"
+  + "*a hook*a chainsaw*a scythe*an eggbeater*hedge-clippers*a blowtorch*a rubber duck"
   + "*a pizza box*a hoola hoop*a beer keg*a Rubik's Cube*a cigar*a megaphone"
-  + "*a pineapple*an axe*a trebuchet*a bottle of Jim Beam*with a bong";
+  + "*a pineapple*an axe*a trebuchet*a bottle of Jim Beam*a bong*a baseball bat";
 
 // define substitution - body part to be substituted
-g3b2 = "for a hand*for a foot*for a head";
+g3b2 = "for a hand*for a foot*for a nose*for a head";
